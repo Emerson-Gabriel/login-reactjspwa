8ba1ec4ebdbb8997510c195549b3f746f26a9b63
Emerson <emersong730@gmail.com>
2023-11-17 09:57:04 -0300

    up

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,24 +7,24 @@ import Contato from "../pages/Contato";
 import ContatoNovo from "../pages/ContatoNovo";
 
 const Private = ({ Item }) => {
-  const { signed } = useAuth();
-  return signed > 0 ? <Item /> : <Signin />;
+    const { signed } = useAuth();
+    return signed > 0 ? <Item /> : <Signin />;
 };
 
 const RoutesApp = () => {
-  return (
-    <BrowserRouter>
-      <Fragment>
-        <Routes>
-          <Route exact path="/home" element={<Private Item={Home} />} />
-          <Route exact path="/contatos" element={<Private Item={Contato} />} />
-          <Route exact path="/contato/novo" element={<Private Item={ContatoNovo} />} />
-          <Route path="/" element={<Signin />} />
-          <Route path="*" element={<Signin />} />
-        </Routes>
-      </Fragment>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Fragment>
+                <Routes>
+                    <Route exact path="/home" element={<Private Item={Home} />} />
+                    <Route exact path="/contatos" element={<Private Item={Contato} />} />
+                    <Route exact path="/contato/novo" element={<Private Item={ContatoNovo} />} />
+                    <Route path="/" element={<Signin />} />
+                    <Route path="*" element={<Signin />} />
+                </Routes>
+            </Fragment>
+        </BrowserRouter>
+    );
 };
 
 export default RoutesApp;
